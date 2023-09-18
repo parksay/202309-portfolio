71f6592a2bfa2d2cce25bfd79f5b0c18d5c44111
parksay_laptop <hellobigworld.parksay@gmail.com>
2023-09-18 10:09:43 +0900

    initial commit

---
+++ src/main/java/simple/myboard/myprac/MypracApplication.java
@@ -0,0 +1,13 @@
+package simple.myboard.myprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simple/myboard/myprac/MypracApplicationTests.java
@@ -0,0 +1,13 @@
+package simple.myboard.myprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
