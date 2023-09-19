bfa54dbe295010ed0ad46aaa04f07658102e6195
parksay_laptop <hellobigworld.parksay@gmail.com>
2023-09-19 11:31:32 +0900

    resources files

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,53 @@
+
+INSERT INTO TB_MEMBER  (
+    user_id,
+    user_psw,
+    user_name
+) VALUES (
+    'testid01',
+    'testpsw01',
+    'testname01'
+);
+
+INSERT INTO TB_MEMBER  (
+    user_id,
+    user_psw,
+    user_name
+) VALUES (
+    'testid02',
+    'testpsw02',
+    'testname02'
+);
+
+
+INSERT INTO TB_MEMBER  (
+    user_id,
+    user_psw,
+    user_name
+) VALUES (
+    'testid03',
+    'testpsw03',
+    'testname03'
+);
+
+
+INSERT INTO TB_ARTICLE (
+    member_seq,
+    title,
+    contents
+) VALUES (
+    0,
+    'testtitle01',
+    'testcontents01'
+);
+
+
+INSERT INTO TB_ARTICLE (
+    member_seq,
+    title,
+    contents
+) VALUES (
+    1,
+    'testtitle02',
+    'testcontents02'
+);
